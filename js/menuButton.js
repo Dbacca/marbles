67159ec2a817e743fc961efb1523d654d6952874
Drew Since <cplsince@gmail.com>
2016-08-26 02:00:21 +0700

    team page js linked and z-indexes fixed

--- js/menuButton.js
+++ js/menuButton.js
@@ -4,6 +4,7 @@ $(function() {
       $('.nav').toggleClass('nav--active')
       $('.navItem').toggleClass('navItem--active')
       $('.logo').toggleClass('logo--active')
+      $('.label').toggleClass('label--active')
       console.log('menu button clicked');
     })
 });
